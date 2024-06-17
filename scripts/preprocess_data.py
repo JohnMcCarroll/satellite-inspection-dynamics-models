8053e514990de14616339e5a3897b9f40fc50482
John McCarroll <john.mccarroll.ext@afresearchlab.com>
2024-06-17 19:49:56 -0400

    data preprocessing

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,73 @@
+import pickle
+import pandas as pd
+import numpy as np
+import math
+
+
+# load the DataFrame
+def load_dataframe(file_path):
+    return pd.read_pickle(file_path)
+
+# calculate mean and standard deviation across all elements
+def calculate_stats(df):
+    all_obs = None
+    for traj in df['Trajectory']:
+        for state in traj:
+            if all_obs is None:
+                all_obs = np.array([state[0]])
+            else:
+                all_obs = np.concatenate((all_obs, [state[0]]), axis=0)
+    mean = np.mean(all_obs, axis=0)
+    std = np.std(all_obs, axis=0)
+    return mean, std
+
+# z-score the arrays
+def z_score_array(array, mean, std):
+    return (array - mean) / std
+
+# z-score the entire DataFrame
+def z_score_dataframe(df, mean, std):
+    z_scored_trajectories = []
+    for traj in df['Trajectory']:
+        processed_traj = []
+        for state in traj:
+            processed_obs = z_score_array(state[0], mean, std)
+            # add sun angle conversion
+            sun_angle = state[0][-1]
+            x = math.cos(sun_angle)
+            y = math.sin(sun_angle)
+            processed_obs[-1] = x
+            processed_obs = np.append(processed_obs, y)
+            state = (processed_obs, state[1], state[2], state[3], state[4])
+
+            processed_traj.append(state)
+        z_scored_trajectories.append(processed_traj)
+    return pd.DataFrame({'Trajectory': z_scored_trajectories})
+
+# load data
+file_path1 = "data/ppo_dataset1.pkl"
+file_path2 = "data/ppo_dataset2.pkl"
+file_path3 = "data/random_dataset.pkl"
+file_path4 = "data/random_dataset3.pkl"
+file_path5 = "data/random_dataset4.pkl"
+file_path6 = "data/test_dataset.pkl"
+
+data_files = [
+    file_path1,
+    file_path2,
+    file_path3,
+    file_path4,
+    file_path5,
+    file_path6
+]
+
+for file_path in data_files:
+    df = load_dataframe(file_path6)
+
+    mean, std = calculate_stats(df)
+
+    z_scored_df = z_score_dataframe(df, mean, std)
+
+    new_file = file_path.split('/')[-1]
+    z_scored_df.to_pickle(f'processed_{new_file}')
+
